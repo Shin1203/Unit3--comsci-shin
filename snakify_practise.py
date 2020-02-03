9dd6865f2780753d857cd6d4734cb4330b553aff
Shin M <54606968+Shin1203@users.noreply.github.com>
2020-02-04 02:12:52 +0900

    Update snakify_practise.py

--- snakify_practise.py
+++ snakify_practise.py
@@ -2,11 +2,46 @@ INPUT PRINT AND NUMBERS
   Sum of Three Numbers 
   `` 
   # This program reads two numbers and prints their sum:
-a = int(input())
-b = int(input())
-c = int(input())
-print(a + b + c)
+  a = int(input())
+  b = int(input())
+  c = int(input())
+  print(a + b + c)
 
-# Can you change it so it can add three numbers?
+  # Can you change it so it can add three numbers?
 
   ``
+    
+    Square
+    ``
+    # Read an integer:
+  a = int(input())
+  # Read a float:
+  b = int(a * a)
+  # Print a value:
+  print(b)
+  
+    ``
+    
+    Apple Sharing
+    ``
+   # Read the numbers like this:
+   N = int(input())
+   K = int(input())
+   # Print the result with print()
+
+   # Example of division, integer division and remainder:
+   print(K // N)
+   print(K % N)
+    ``
+    
+    Previous and Next
+    ``
+    # Read an integer:
+  a = int(input())
+  b = int(a+1)
+  c = int(a-1)
+  # Print a value:
+  print("The next number for the number", str(a) ,"is", str(b),)
+  print("The previous number for the number", str(a) ,"is", str(c),)
+
+    ``
