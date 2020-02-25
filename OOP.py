7f6f61e940ca449ae4d75e38502af259c7ce5822
Shin M <54606968+Shin1203@users.noreply.github.com>
2020-02-25 23:02:15 +0900

    Update OOP.py

--- OOP.py
+++ OOP.py
@@ -1,17 +1,47 @@
-#
+
 # What’s a class?
+# A class is a user-defined data structure that can store information
+
 # What’s an instance?
+# An instance of a class has stored variables- the class is the structure and the instance a variable in that structure. 
+
 # What’s the relationship between a class and an instance?
-What’s the Python syntax used for defining a new class?
-What’s the spelling convention for a class name?
-How do you instantiate, or create an instance of, a class?
-How do you access the attributes and behaviors of a class instance?
-What’s a method?
-What’s the purpose of self?
-What’s the purpose of the __init__ method?
-Describe how inheritance helps prevent code duplication.
-Can child classes override properties of their parents?
-#
+# An instance is made out of a class, the class defines all the properties it's instances can have
+
+# What’s the Python syntax used for defining a new class?
+ class x:
+        def __init__(self, y, z, m):
+            self.y = y
+            self.z = z
+            self.m = m
+ pass
+    
+# What’s the spelling convention for a class name?
+# capitalize first letter, e.g- Dog, Cat, Milk
+
+# How do you instantiate, or create an instance of, a class?
+instance = classname(argument properties)
+
+# How do you access the attributes and behaviors of a class instance?
+instance.attribute
+instance.behavior(argument of function)
+
+# What’s a method?
+# functions inside of a class that define the behaviours of the objects
+
+# What’s the purpose of self?
+# self is an instance of the class, it is used so that instances of a class can have different variables. if you put Classname.attribute, 
+#the variable is a class attribute, not an instance attribute- A.k.a all instances of the class will have the same attribute.
+
+# What’s the purpose of the __init__ method?
+# initializes object's initial attributes
+
+# Describe how inheritance helps prevent code duplication.
+# if you wanted to make classes that had some similarities but some differences, you dont have to make complete differnt classes, but put
+# them under a parent class that stores their similarities, so that you don't have to rewrite the similarities.
+
+# Can child classes override properties of their parents?
+# yes.
 
 
 
